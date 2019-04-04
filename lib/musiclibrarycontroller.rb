1cc6631e09ed2db3b6f0473a832992e271316256
Jay Sokolowski <jaysoko@gmail.com>
2019-04-04 19:31:34 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -33,6 +33,8 @@ class MusicLibraryController
       list_songs_by_artist
     when "list genre"
       list_songs_by_genre
+    when "play song"
+       play_song
     else
       puts "Jaybird!"
     end
